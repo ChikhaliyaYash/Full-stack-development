02b152f94978c954a6a570b86e565c6e6d9584af
ChikhaliyaYash <chikhaliyayash899@gmail.com>
2022-08-06 07:27:50 -0700

    Done

--- Practical 3/src/index.js
+++ Practical 3/src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./components/App";
+import temp from "./components/temp";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <temp />
   </React.StrictMode>
 );
 
