324cb51225dbdb863ab423d1777b9ad470df4df6
ChikhaliyaYash <chikhaliyayash899@gmail.com>
2022-08-28 21:30:41 +0530

    Done

--- practical2/src/pages/Home.js
+++ practical2/src/pages/Home.js
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div>
       <h1>Hello</h1>
-      <h1>Yash chikhaliya(D21DCS149)</h1>
+      <center><h1>Yash Chikhaliya(D21DCS149)</h1></center>
     </div>
   );
 }
