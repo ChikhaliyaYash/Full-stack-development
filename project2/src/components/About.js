29e8c2bc004c9f744c448a8bb2aaea01de537cc9
ChikhaliyaYash <chikhaliyayash899@gmail.com>
2022-09-26 23:52:11 +0530

    Done

--- project2/src/components/About.js
+++ project2/src/components/About.js
@@ -31,7 +31,7 @@ export default class About extends Component {
                 <p>
                   Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
                   velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                  culpa qui officia deserunt mollit anim id est laborum
+                  culpa mqui officia deserunt mollit anim id est laborum
                 </p>
               </div>
   
